e05d7c4e9bddb328a635ad45e14090a1183dd501
Gaurav2002-developer <2002gaurav.chaturvedi@gmail.com>
2025-08-16 22:34:04 +0000

    Pulled templates, cred.env from database branch

--- Freelix_site/core/models.py
+++ Freelix_site/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Movies(models.Model):
+    Genre_Choices = [
+        ('action', 'Action'),
+        ('comdedy', 'Comedy'),
+        ('drama', 'Drama'),
+        ('horror','Horror')
+
+    ]
+
+    uu_id = models.UUIDField(default = uuid.uuid4)
+    title = models.CharField(max_length = 225)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length = 100, choices = Genre_Choices)
+    length = models.PositiveIntegerField()
+    image_card = models.ImageField(upload_to = 'movie_images/')
+    image_cover = models.ImageField(upload_to = 'movie_images/')
+    video = models.FileField(upload_to = 'movie_videos/')
+    movie_views = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.title
+    # Create your models here.
