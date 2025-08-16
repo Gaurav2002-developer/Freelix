22e86409f597158b46273349e52630a6b3e769a3
Shubham10122 <shubham.bhawani402@gmail.com>
2025-08-17 03:17:10 +0530

    supabase connected

--- Freelix_site/Freelix_site/settings.py
+++ Freelix_site/Freelix_site/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, "cred.env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -77,17 +81,34 @@ WSGI_APPLICATION = 'Freelix_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'djangodatabase',
-        'HOST': 'localhost',  # or your server IP
-        'PORT': '5430',
-        'OPTIONS' : {
-            'options': '-c search_path=myschema' 
-        } # Use 'require' for SSL connections
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'myprojectuser',
+#         'PASSWORD': 'djangodatabase',
+#         'HOST': 'localhost',  # or your server IP
+#         'PORT': '5430',
+#         'OPTIONS' : {
+#             'options': '-c search_path=myschema' 
+#         } # Use 'require' for SSL connections
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -129,7 +150,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
